51063c5ff068edff9eed05e28cb4cddcd55c426e
ClaudioScordari <scordari22@gmail.com>
2024-01-17 16:33:50 +0100

    Completato esercizio

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+// Importazione file 
+import AppTitle from './components/AppTitle.vue';
+
+    export default {
+        data() {
+            return { 
+                count: 0 
+            }
+        },
+        // Dichiarazione dei componenti
+        components: {
+            AppTitle
+        }
+    }
+</script>
+
+<template>
+    <div>
+        <!-- Utilizzo del componente -->
+        <div>
+            <h1>
+                <AppTitle/>
+            </h1>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
